3bcb45a7f0a0a17c2f8ed757a0efa551ac071fbf
utopiasz <s3ndyy3@gmail.com>
2026-05-21 14:38:36 +0200

    ci: tests fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        if (app()->environment('testing')) {
-            $middleware->web(remove: [
-                \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
-            ]);
-        }
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Str;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $token = Str::random(40);
+
+        $this->withSession(['_token' => $token]);
+        $this->withHeader('X-CSRF-TOKEN', $token);
+    }
 }
